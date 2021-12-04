46c088e33343ce3d5da1dae43f4c9692775429ff
Rowan DeLong <rowanwinzer@gmail.com>
2021-12-04 14:30:35 -0700

    Add allowed url for CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins 'https://thumbtack-ui.herokuapp.com'
 
     resource '*',
       headers: :any,
