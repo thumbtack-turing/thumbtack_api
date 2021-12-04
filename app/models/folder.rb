b021c93eb35dd71f518d9e6a9d661bc4138534ae
Rowan DeLong <rowanwinzer@gmail.com>
2021-12-04 14:10:53 -0700

    Add association for child folders

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,8 @@
 class Folder < ApplicationRecord
   belongs_to :user
-  belongs_to :parent, class_name: 'Folder'
+  belongs_to :parent, class_name: 'Folder', optional: true
   has_many :folder_resources, dependent: :destroy
   has_many :resources, through: :folder_resources
+  has_many :folders, class_name: "Folder", foreign_key: :parent_id
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_193202) do
+ActiveRecord::Schema.define(version: 2021_12_04_210228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
