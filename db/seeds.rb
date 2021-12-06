67f4d72301e75962f532006fe03b0fe1e2b74a58
Erika Kischuk <erika.kischuk@gmail.com>
2021-12-06 15:52:52 -0700

    Add user and base folder to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(name: 'Erika', email: 'eak@example.com')
+user.folders.create(name: 'erika_base', base: true)
