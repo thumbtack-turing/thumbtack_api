b9c52fc1b572c9857a968cf034db1ecfd8f804c7
Jamie Pace <jamiejpace@gmail.com>
2021-12-05 17:36:35 -0700

    Refactor formatting of user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User do
+RSpec.describe User, type: :model do 
   describe 'relationships' do
     it { should have_many(:folders) }
     it { should have_many(:resources).through(:folders) }
