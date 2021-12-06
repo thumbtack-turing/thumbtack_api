8d70d7cd77e6498715669fa376a8dab06dca26e6
Jamie Pace <jamiejpace@gmail.com>
2021-12-05 17:41:52 -0700

    Add relationship testing for folder_resource model

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,6 +3,5 @@ class Folder < ApplicationRecord
   belongs_to :parent, class_name: 'Folder', optional: true
   has_many :folder_resources, dependent: :destroy
   has_many :resources, through: :folder_resources
-  has_many :folders, class_name: "Folder", foreign_key: :parent_id
-
+  has_many :folders, class_name: 'Folder', foreign_key: :parent_id
 end
