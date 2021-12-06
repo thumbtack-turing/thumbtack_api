f2130177eac29b882b61d280a15b22fbde4770f8
Jamie Pace <jamiejpace@gmail.com>
2021-12-05 20:05:06 -0700

    Create mutation for createUser

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,15 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: 'An example field added by the generator'
-    def test_field
-      'Hello World'
+
+    field :create_user, UserType, null: true, description: 'Create a user' do
+      argument :name, String, required: true
+      argument :email, String, required: true
+    end
+
+    def create_user(name:, email:)
+      user = User.create(name: name, email: email)
+      user.folders.create(name: "Base Folder", base: true)
+      user
     end
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,9 @@ class Types::UserType < Types::BaseObject
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
+    field :base_folder, Types::FolderType, null: false
+
+    def base_folder
+      object.base_folder
+    end
 end
