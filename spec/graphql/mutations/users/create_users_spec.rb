d11b08234bcf6ca5304a78f7c7be79a82ef2dda3
Jamie Pace <jamiejpace@gmail.com>
2021-12-06 17:43:41 -0700

    Add to test for create user mutation

--- spec/graphql/mutations/users/create_users_spec.rb
+++ spec/graphql/mutations/users/create_users_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe Types::MutationType do
   describe 'create users mutation' do
     it 'creates a user' do
       expect(User.count).to eq(0)
-      ThumbtackApiSchema.execute(query).as_json
+      result = ThumbtackApiSchema.execute(query).as_json
       expect(User.count).to eq(1)
+      expect(result["data"]["createUser"]["name"]).to eq("Odell Stang")
+      expect(result["data"]["createUser"]["email"]).to eq("odellthedog@test.com")
     end
   end
 
