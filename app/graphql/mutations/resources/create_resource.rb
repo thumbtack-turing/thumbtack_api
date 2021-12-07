6a81c9e0962f08300763dd3722de0a4520a859b7
Rowan DeLong <rowanwinzer@gmail.com>
2021-12-06 19:42:51 -0700

    Add createResource mutation

---
+++ app/graphql/mutations/resources/create_resource.rb
@@ -0,0 +1,33 @@
+module Mutations
+    module Resources
+      class CreateResource < ::Mutations::BaseMutation
+        argument :name, String, required: true
+        argument :url, String, required: true
+        argument :folder_id, ID, required: true
+
+        def resolve(name:, url:, folder_id:)
+          img = get_image(url)
+          new_res = Resource.create(
+            name: name,
+            url: url,
+            image: img
+          )
+        
+          new_fr = FolderResource.create(
+              folder_id: folder_id, 
+              resource_id: new_res.id
+            ) 
+          
+          Folder.find(folder_id)
+        end
+  
+        private
+
+        def get_image(url)
+            page = MetaInspector.new(url)
+            page.images.best 
+        end
+  
+      end
+    end
+  end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
+
     field :create_user, Types::UserType, mutation: Mutations::Users::CreateUser
     field :create_folder, mutation: Mutations::Folders::CreateFolder
+    field :create_resource, Types::FolderType, mutation: Mutations::Resources::CreateResource
+    
   end
 end
