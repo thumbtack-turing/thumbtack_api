3b12db702277d9f537416834ef20c43273a6c75d
Jamie Pace <jamiejpace@gmail.com>
2021-12-06 17:44:04 -0700

    Refactor test formatting

--- spec/graphql/mutations/users/create_users_spec.rb
+++ spec/graphql/mutations/users/create_users_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Types::MutationType do
   describe 'create users mutation' do
     it 'creates a user' do
       expect(User.count).to eq(0)
+
       result = ThumbtackApiSchema.execute(query).as_json
+      
       expect(User.count).to eq(1)
       expect(result["data"]["createUser"]["name"]).to eq("Odell Stang")
       expect(result["data"]["createUser"]["email"]).to eq("odellthedog@test.com")
