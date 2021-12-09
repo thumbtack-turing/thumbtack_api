914a82c362bb607d1619e04f123695c6120d43a4
Jamie Pace <jamiejpace@gmail.com>
2021-12-09 07:57:54 -0700

    Add test for query with filePath returned

--- spec/graphql/folders/get_folder_spec.rb
+++ spec/graphql/folders/get_folder_spec.rb
@@ -10,9 +10,9 @@ RSpec.describe Types::QueryType do
       sub_folder1 = user1.folders.create!(name: "sub1", base: false, user_id: user1.id, parent_id: base_folder.id)
       sub_folder2 = Folder.create!(name: "sub2", base: false, parent_id: base_folder.id, user_id: user1.id)
       resource1 = base_folder.resources.create(name: "resource1", url: "123.com", image: "123.image")
-      
+
       result = ThumbtackApiSchema.execute(query(base_folder.id)).as_json
-      
+
       expect(result["data"]["getFolder"]["name"]).to eq("Root")
       expect(result["data"]["getFolder"]["base"]).to eq(true)
       expect(result["data"]["getFolder"]["id"]).to eq(base_folder.id.to_s)
@@ -29,6 +29,20 @@ RSpec.describe Types::QueryType do
       expect(result["data"]["getFolder"]["childResources"].first["name"]).to eq('resource1')
       expect(result["data"]["getFolder"]["childResources"].first["url"]).to eq("123.com")
       expect(result["data"]["getFolder"]["childResources"].first["image"]).to eq("123.image")
+    end
+
+    it 'can return a file path' do
+      user1 = User.create( name: "Jamie", email: "jamie@test.com")
+
+      base_folder = user1.folders.create!(name: "Base", base: true)
+
+      sub_folder1 = user1.folders.create!(name: "GraphQL", base: false, user_id: user1.id, parent_id: base_folder.id)
+      sub_folder2 = Folder.create!(name: "Mutations", base: false, parent_id: sub_folder1.id, user_id: user1.id)
+      sub_folder3 = Folder.create!(name: "Tutorial videos", base: false, parent_id: sub_folder2.id, user_id: user1.id)
+
+      result = ThumbtackApiSchema.execute(query2(sub_folder3.id)).as_json
+
+      expect(result["data"]["getFolder"]["filePath"]).to eq("/Base/GraphQL/Mutations/Tutorial-videos")
     end
   end
 
@@ -52,6 +66,33 @@ RSpec.describe Types::QueryType do
             url
             image
             createdAt
+        }
+      }
+    }
+    GQL
+  end
+
+  def query2(id)
+    <<~GQL
+    {
+      getFolder (id: "#{id}") {
+        id
+        name
+        base
+        filePath
+        parentId
+        childFolders {
+          id
+          name
+          base
+          parentId
+        }
+        childResources {
+          id
+          name
+          url
+          image
+          createdAt
         }
       }
     }
