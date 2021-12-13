1e6c2e3fa6d11038f5b91ab57c0792b63ad3c4a7
Erika Kischuk <erika.kischuk@gmail.com>
2021-12-13 15:33:56 -0700

    Add rescue for RecordNotFound

--- app/graphql/mutations/folders/delete_folder.rb
+++ app/graphql/mutations/folders/delete_folder.rb
@@ -2,10 +2,13 @@ class Mutations::Folders::DeleteFolder < GraphQL::Schema::Mutation
   argument :id, ID, required: true
 
   def resolve(attributes)
-    id = attributes[:id]
-    folder = Folder.find(id)
-    parent = Folder.find(folder.parent_id)
-    folder.destroy
-    parent
+      id = attributes[:id]
+      folder = Folder.find(id)
+      parent = Folder.find(folder.parent_id)
+      folder.destroy
+      parent
+
+  rescue ActiveRecord::RecordNotFound => _e
+    GraphQL::ExecutionError.new("Invalid folder id.")
   end
 end
