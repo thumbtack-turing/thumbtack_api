c3a7d14bb930274ee2ebffc67927829453ad6862
Erika Kischuk <erika.kischuk@gmail.com>
2021-12-13 20:39:00 -0700

    Add rescue for meta inspector gem request error and timeout error

--- app/graphql/mutations/resources/create_resource.rb
+++ app/graphql/mutations/resources/create_resource.rb
@@ -7,27 +7,31 @@ module Mutations
 
         def resolve(name:, url:, folder_id:)
           img = get_image(url)
-         
+
           new_res = Resource.new(name: name, url: url, image: img, folder_id: folder_id)
-    
+
           if new_res.save
             Folder.find(folder_id)
           else
             raise GraphQL::ExecutionError, new_res.errors.full_messages.join(", ")
           end
+        rescue MetaInspector::RequestError => _e
+          GraphQL::ExecutionError.new("Invalid url.")
+        rescue MetaInspector::TimeoutError => e
+          GraphQL::ExecutionError.new("Url cannot be reached.")
         end
 
         private
 
         def get_image(url)
-          page = MetaInspector.new(url)
-          img = page.images.best 
+          page = MetaInspector.new(url, :connection_timeout => 10)
+          img = page.images.best
           if img.nil?
             img = "https://www.oiml.org/en/ressources/icons/link-icon.png/image_preview"
           end
           img
         end
-  
+
       end
     end
   end
